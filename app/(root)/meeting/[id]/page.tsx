e95d46fdb824ecebb81185e604a9cd06934493f3
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-10-21 21:42:18 +0300

    meetings calls setup right now

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,6 +1,9 @@
+"use client";
+import { useUser } from "@clerk/nextjs";
 import React from "react";
 
 const Meeting = ({ params }: { params: { id: string } }) => {
+	const { user, isLoaded } = useUser();
 	return (
 		<div>
 			Meeting Room id: #{params.id}
