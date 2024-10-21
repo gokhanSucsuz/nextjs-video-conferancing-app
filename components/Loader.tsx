d790ce72a7ff4f03418021fec089dcef64a0711e
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-10-21 22:24:01 +0300

    some bugs are fixed

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -2,16 +2,9 @@ import Image from "next/image";
 import React from "react";
 
 const Loader = () => {
-	return (
-		<div className="flex jstify-center items-center w-full">
-			<Image
-				src="/icons/loading-circle.svg"
-				alt="loading"
-				width={64}
-				height={64}
-			/>
-		</div>
-	);
+	return <div className="flex justify-center items-center w-full h-screen">
+			<Image src="/icons/loading-circle.svg" alt="loading" width={64} height={64} />
+		</div>;
 };
 
 export default Loader;
